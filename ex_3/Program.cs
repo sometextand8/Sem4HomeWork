972d4718ca5806365333a242195cb891d1197bc6
Wasil <death888080@gmail.com>
2022-03-25 23:25:29 +0100

    ex_3

---
+++ ex_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+
+void CreateAnArray(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(101);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+int[] Array = new int[8];
+
+CreateAnArray(Array);
