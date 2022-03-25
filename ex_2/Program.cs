1f9f5a35f885b129f2eed2364663d00e86e2062b
Wasil <death888080@gmail.com>
2022-03-25 23:24:14 +0100

    ex_2

---
+++ ex_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+void LengthOfnum(string num)
+{
+    int len = num.Length;
+    Console.WriteLine($"В числе {num}, кол. чисел = {len}.");
+
+}
+
+
+LengthOfnum("123123123");
+
+
+// Задача 2 
+
+
+void LenOfInput(int number)
+{
+    int showNum = number;
+    int count = 0;
+
+    while (number % 10 > 0)
+    {
+        number /= 10;
+        count ++;
+    }
+
+    Console.WriteLine($"В числе {showNum}, кол.во цифр = {count}.");
+
+}
+
+LenOfInput(76);
